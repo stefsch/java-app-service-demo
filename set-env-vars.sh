2d362e6ca11e47b3f5461622548452fcaa857305
Jason Freeberg <jr.freeberg@comcast.net>
2018-10-28 16:45:50 -0700

    blaahhhh

--- set-env-vars.sh
+++ set-env-vars.sh
@@ -1,9 +1,7 @@
-# Changes to this file won't be tracked by git.
 
 export DOCUMENTDB_URI=<replace-with-your-URI>
 export DOCUMENTDB_KEY=<replace-with-your-primaryMasterKey>
 export DOCUMENTDB_DBNAME=<replace-with-your-db-name>
 
-
-
-... test!
+# Untrack this file to keep your env vars out of git ;)
+git update-index --assume-unchanged set-env-vars.sh
