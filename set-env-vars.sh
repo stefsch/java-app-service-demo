46093ebca457aea1d69ae4ee347322a4c0b3dbf8
Jason Freeberg <jr.freeberg@comcast.net>
2019-09-19 15:58:19 -0700

    remove files from branch

--- set-env-vars.sh
+++
@@ -1,7 +0,0 @@
-
-export DOCUMENTDB_URI=<replace-with-your-URI>
-export DOCUMENTDB_KEY=<replace-with-your-primaryMasterKey>
-export DOCUMENTDB_DBNAME=<replace-with-your-db-name>
-
-# Untrack this file to keep your env vars out of git ;)
-git update-index --assume-unchanged set-env-vars.sh
